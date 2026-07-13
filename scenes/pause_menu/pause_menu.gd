8af30b6674d7fc267f2c38cc3b2db6ced607b939
DevGreem <ian.pichardo@hotmail.com>
2026-07-12 21:44:22 -0400

    fix: Fixed PauseMenu bug on win a level
    You could win and simultaneously pause the game, causing the menus to overlap

--- scenes/pause_menu/pause_menu.gd
+++ scenes/pause_menu/pause_menu.gd
@@ -1,14 +1,19 @@
 extends CenterContainer
 
+var _paused_by_self := false
+
 func _input(event: InputEvent):
 	
-	if event.is_action_pressed("pause"):
-		if self.visible:
-			_on_resume_button_pressed()
-		else:
-			get_tree().paused = true
-			show()
+	if not get_tree().paused or _paused_by_self:
+		if event.is_action_pressed("pause"):
+			if self.visible:
+				_on_resume_button_pressed()
+			else:
+				get_tree().paused = true
+				show()
+				_paused_by_self = true
 
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 	hide()
+	_paused_by_self = false
